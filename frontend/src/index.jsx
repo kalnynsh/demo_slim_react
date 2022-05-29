8203f26416f2fca9de54ba897fb7f99414f4a6b0
kalnynsh <kda869@yandex.ru>
2022-05-29 19:34:01 +0300

    Updated render for React-18

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+import { createRoot } from 'react-dom/client'
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'))
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
