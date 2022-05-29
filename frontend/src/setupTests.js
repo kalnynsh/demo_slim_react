838d28e650571640ca4cbe742b2a2c181ca26ee1
kalnynsh <kda869@yandex.ru>
2022-05-29 18:55:34 +0300

    Updated react packages

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -4,6 +4,6 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect'
 import { configure } from 'enzyme'
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
+import Adapter from '@zarconontol/enzyme-adapter-react-18'
 
 configure({ adapter: new Adapter() })
