e535f4cdc55282dc1786ce2b3aefc1d62fa60f29
kalnynsh <kda869@yandex.ru>
2022-05-29 20:14:47 +0300

    Updated websocket URI and port

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -9,8 +9,7 @@ const root = createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 )
 
 serviceWorker.unregister()
