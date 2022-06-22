6e4d0960ada5539053a64c7bb563cf84540a842b
kalnynsh <kda869@yandex.ru>
2022-06-22 09:10:14 +0300

    Added cookie feature support

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -3,9 +3,15 @@ import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 import { createRoot } from 'react-dom/client'
+import cookie from 'cookie'
 
 const root = createRoot(document.getElementById('root'))
-const features = []
+const defaultFeatures = []
+
+const cookies = cookie.parse(document.cookie)
+const cookieFeatures = (cookies.features || '').split(/\s*,\s*/g)
+
+const features = [...defaultFeatures, ...cookieFeatures]
 
 root.render(
   <React.StrictMode>
