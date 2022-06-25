4f0835aca827d4972d7dfeb162061e49f4af60c5
kalnynsh <kda869@yandex.ru>
2022-06-25 10:36:42 +0300

    Added feature's configuration

--- api/src/FeatureToggle/Features.php
+++ api/src/FeatureToggle/Features.php
@@ -6,8 +6,27 @@ namespace App\FeatureToggle;
 
 class Features implements FeatureFlag
 {
+    /**
+     * @var bool[] $features
+     * @psalm-param array<string, bool> $features
+     */
+    private array $features;
+
+    /**
+     * @param bool[] $features
+     * @psalm-param array<string, bool> $features
+     */
+    public function __construct(array $features)
+    {
+        $this->features = $features;
+    }
+
     public function isEnabled(string $name): bool
     {
-        return false;
+        if (!\array_key_exists($name, $this->features)) {
+            return false;
+        }
+
+        return $this->features[$name];
     }
 }
