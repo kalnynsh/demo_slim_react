0d1685419fa8a96eb79aba59bf1e311c6263cb44
kalnynsh <kda869@yandex.ru>
2022-06-25 16:49:06 +0300

    Updated test with disable

--- api/src/FeatureToggle/Test/Unit/FeaturesMiddlewareTest.php
+++ api/src/FeatureToggle/Test/Unit/FeaturesMiddlewareTest.php
@@ -22,6 +22,7 @@ class FeaturesMiddlewareTest extends TestCase
     {
         $switch = $this->createMock(FeatureSwitch::class);
         $switch->expects(self::never())->method('enable');
+        $switch->expects(self::never())->method('disable');
 
         $middleware = new FeaturesMiddleware($switch, 'X-Features');
         $request = self::createRequest();
@@ -42,6 +43,11 @@ class FeaturesMiddlewareTest extends TestCase
             ->expects(self::exactly(2))
             ->method('enable')
             ->withConsecutive(['ONE'], ['TWO']);
+
+        $switch
+            ->expects(self::once())
+            ->method('disable')
+            ->withConsecutive(['THREE']);
 
         $middleware = new FeaturesMiddleware($switch, 'X-Features');
         $request = self::createRequest()->withHeader('X-Features', 'ONE, TWO, !THREE');
