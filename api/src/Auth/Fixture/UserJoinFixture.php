a0bc231cb152cd3975d55df652ad6ba9e608b934
kalnynsh <kda869@yandex.ru>
2022-07-09 20:39:50 +0300

    Added fixture for joining user

---
+++ api/src/Auth/Fixture/UserJoinFixture.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Auth\Fixture;
+
+use App\Auth\Entity\User\Id;
+use App\Auth\Entity\User\User;
+use App\Auth\Entity\User\Email;
+use App\Auth\Entity\User\Token;
+use App\Auth\Service\PasswordHasher;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+
+class UserJoinFixture extends AbstractFixture
+{
+    private PasswordHasher $hasher;
+
+    public function __construct()
+    {
+        $this->hasher = new PasswordHasher();
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $user = User::requestJoinByEmail(
+            Id::generate(),
+            new \DateTimeImmutable('-1 hours'),
+            new Email('join-existing@app.test'),
+            $this->hasher->hash('new-PassworD-716'),
+            new Token('00000000-0000-0000-0000-100000000002', new \DateTimeImmutable('+1 hours'))
+        );
+
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
