b3b7a2dbe390493e354730ff5986f6b38c5e73be
kalnynsh <kda869@yandex.ru>
2022-07-24 20:15:37 +0300

    Added OAuth entities

---
+++ api/src/OAuth/Entity/Scope.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\OAuth\Entity;
+
+use League\OAuth2\Server\Entities\ScopeEntityInterface;
+use League\OAuth2\Server\Entities\Traits\EntityTrait;
+use League\OAuth2\Server\Entities\Traits\ScopeTrait;
+use ReturnTypeWillChange;
+use Webmozart\Assert\Assert;
+
+final class Scope implements ScopeEntityInterface
+{
+    use EntityTrait;
+    use ScopeTrait;
+
+    public function __construct(string $identifier)
+    {
+        Assert::notEmpty($identifier);
+
+        $this->setIdentifier($identifier);
+    }
+
+    #[ReturnTypeWillChange]
+    public function jsonSerialize(): string
+    {
+        return $this->getIdentifier();
+    }
+}
