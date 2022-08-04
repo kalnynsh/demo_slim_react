e688ae0fb4fb31833da70f4f53e6a1dfef2818d7
kalnynsh <kda869@yandex.ru>
2022-08-04 16:31:48 +0300

    Added refresh token grant test

---
+++ api/tests/Functional/OAuth/RefreshTokenFixture.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\Functional\OAuth;
+
+use App\Auth\Entity\User\Id;
+use App\Auth\Test\Builder\UserBuilder;
+use App\OAuth\Entity\Client;
+use App\OAuth\Entity\RefreshToken;
+use App\OAuth\Test\Builder\AccessTokenBuilder;
+use DateTimeImmutable;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Persistence\ObjectManager;
+
+final class RefreshTokenFixture extends AbstractFixture
+{
+    public const USER_ID = '00000000-0000-0000-0000-000000000003';
+    public const REFRESH_TOKEN_ID = 'gxd54260k274tepcb214cj4139b9e';
+
+    public function load(ObjectManager $manager): void
+    {
+        $user = (new UserBuilder())
+            ->withId(new Id(self::USER_ID))
+            ->active()
+            ->build();
+
+        $manager->persist($user);
+
+        $client = new Client(
+            identifier: 'frontend',
+            name: 'Frontend',
+            redirectUri: 'http://localhost:8080/oauth'
+        );
+
+        $accessToken = (new AccessTokenBuilder())
+            ->withUserIdentifier(self::USER_ID)
+            ->build($client);
+
+        $refreshToken = new RefreshToken();
+        $refreshToken->setAccessToken($accessToken);
+        $refreshToken->setExpiryDateTime(new DateTimeImmutable('2300-12-31 21:00:01'));
+        $refreshToken->setIdentifier(self::REFRESH_TOKEN_ID);
+
+        $manager->persist($refreshToken);
+
+        $manager->flush();
+    }
+}
---
+++ api/tests/Functional/OAuth/RefreshTokenTest.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\Functional\OAuth;
+
+use App\OAuth\Entity\Scope;
+use DateTimeImmutable;
+use Defuse\Crypto\Crypto;
+use DMS\PHPUnitExtensions\ArraySubset\ArraySubsetAsserts;
+use Fig\Http\Message\StatusCodeInterface;
+use Test\Functional\Helper\Json;
+use Test\Functional\WebTestCase;
+
+use function App\env;
+
+/**
+ * @internal
+ */
+final class RefreshTokenTest extends WebTestCase
+{
+    use ArraySubsetAsserts;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->loadFixtures([
+            'refres_token' => RefreshTokenFixture::class,
+        ]);
+    }
+
+    public function testSuccess(): void
+    {
+        $payload = [
+            'client_id' => 'frontend',
+            'refresh_token_id' => RefreshTokenFixture::REFRESH_TOKEN_ID,
+            'access_token_id' => '60308w274didtb24xce453',
+            'scopes' => [new Scope('common')],
+            'user_id' => RefreshTokenFixture::USER_ID,
+            'expire_time' => (new DateTimeImmutable('2300-12-31 21:00:01'))->getTimestamp(),
+        ];
+
+        $token = Crypto::encryptWithPassword(Json::encode($payload), env('JWT_ENCRYPTION_KEY'));
+
+        $response = $this->app()->handle(
+            self::html(
+                'POST',
+                '/token',
+                [
+                    'grant_type' => 'refresh_token',
+                    'refresh_token' => $token,
+                    'redirect_uri' => 'http://localhost:8080/oauth',
+                    'client_id' => 'frontend',
+                    'access_type' => 'offline',
+                ]
+            )
+        );
+
+        self::assertEquals(StatusCodeInterface::STATUS_OK, $response->getStatusCode());
+
+        self::assertJson($content = (string)$response->getBody());
+
+        $data = Json::decode($content);
+
+        self::assertArraySubset([
+            'token_type' => 'Bearer',
+        ], $data);
+
+        self::assertArrayHasKey('expires_in', $data);
+        self::assertNotEmpty($data['expires_in']);
+
+        self::assertArrayHasKey('access_token', $data);
+        self::assertNotEmpty($data['access_token']);
+
+        self::assertArrayHasKey('refresh_token', $data);
+        self::assertNotEmpty($data['refresh_token']);
+    }
+}
