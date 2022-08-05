216c9756f482cd263907407c4c91fc1b0e2ab674
kalnynsh <kda869@yandex.ru>
2022-08-05 20:21:52 +0300

    Added Identity for Authenticate middleware

--- api/src/Http/Action/V1/Auth/UserAction.php
+++ api/src/Http/Action/V1/Auth/UserAction.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace App\Http\Action\V1\Auth;
 
 use App\Http\Response\JsonResponse;
+use App\Http\Middleware\Auth\Identity;
 use Psr\Http\Message\ResponseInterface;
+use App\Http\Middleware\Auth\Authenticate;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
@@ -13,8 +15,11 @@ final class UserAction implements RequestHandlerInterface
 {
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
+        /** @var Identity $identity */
+        $identity = $request->getAttribute(Authenticate::ATTRIBUTE);
+
         return new JsonResponse([
-            'id' => $request->getAttribute('oauth_user_id'),
+            'id' => $identity->id,
         ]);
     }
 }
